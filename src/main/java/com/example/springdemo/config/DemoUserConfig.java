222c6eb16e8b8c07dce4dfad824a4422b16ab4b1
chenming <294225707@qq.com>
2021-03-10 16:18:37 +0800

    屏蔽@EnableWebMvc注解，修复静态资源失效

--- src/main/java/com/example/springdemo/config/DemoUserConfig.java
+++ src/main/java/com/example/springdemo/config/DemoUserConfig.java
@@ -7,7 +7,6 @@ import com.example.springdemo.resolver.DemoUserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -20,7 +19,7 @@ import java.util.List;
  * @since JDK 1.8
  */
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class DemoUserConfig implements WebMvcConfigurer {
     @Autowired
     private DemoUserArgumentResolver demoUserArgumentResolver;
